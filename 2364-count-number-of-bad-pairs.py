c9acd7c68b73c3247e478bebc249825104c281c5
Mustafa Butt <coolmustafabutt9@gmail.com>
2025-02-09 11:55:29 -0500

    2364 comment update

--- 2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         # key: nums[i] - nums[j] != j - i is equivalent to: nums[i] - i != nums[j] - j
+        # also, sum of unique pairs = n(n-1)/2
         numSubIndex = {}
         for i in range(len(nums)):
             numSubIndex[nums[i] - i] = numSubIndex.get(nums[i] - i, 0) + 1
