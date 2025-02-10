75e4d2f9ec384be8b6a7b4a7114baf2163988600
yamini981 <coolmustafabutt9@gmail.com>
2025-02-10 18:51:46 -0500

    3174 removed superfluous line

--- 3174-clear-digits.py
+++ 3174-clear-digits.py
@@ -1,7 +1,6 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
         # might be better to reconstruct the string
-        digitPointer = 0
         ret = ""
         digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
         for i in range(len(s)):
