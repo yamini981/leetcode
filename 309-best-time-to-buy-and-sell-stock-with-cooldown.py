a27c3a480af0f8b3a45a71f227639e16672b999a
Mustafa Butt <coolmustafabutt9@gmail.com>
2025-02-21 13:38:03 -0500

    309 Comments

--- 309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,8 +7,14 @@ class Solution:
 
         for price in prices:
             oldSold = sold
+            
+            # if our state is sold, that means we just sold what we hold
             sold = held + price
+
+            # if our state is held, we are either already holding, or we just bought
             held = max(held, reset - price)
+
+            # if our state is reset, we are either just resting again, or we just sold
             reset = max(reset, oldSold)
 
         return max(sold, reset)
