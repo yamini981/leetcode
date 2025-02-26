432eb14f24bf67d4bda11018e7754f133ac93037
yamini981 <coolmustafabutt9@gmail.com>
2025-02-26 15:19:56 -0500

    207. Course Schedule (Better Answer - Kahn's Algo)

--- 207-course-schedule.py
+++ 207-course-schedule.py
@@ -1,30 +1,37 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseDict = {}
+        # graph problem - you have a bunch of prerequisites
+        # Editorial explains the algo very well
+        # essentially, keep track of each node and its neighbors
+        # as well as the number of edges going into that node 
+        # Add all of the nodes that don't have any incoming edges
+        # to a queue and run BFS. Each time you pop the queue, increment
+        # a variable that tracks the number of nodes visited
+        # For each neighbor of your popped node decrement
+        # the number of incoming edges since it's been used
+        # and if that node now has 0 incoming edges, add it to the queue
+        # draw it out - makes a bunch of sense 
+        # https://leetcode.com/problems/course-schedule/editorial
+        adj = defaultdict(list)
+
+        indegree = [0] * numCourses
         for prereq in prerequisites:
-            if prereq[0] not in courseDict:
-                courseDict[prereq[0]] = set()
-            courseDict[prereq[0]].add(prereq[1])
+            adj[prereq[1]].append(prereq[0])
+            indegree[prereq[0]] += 1
+
+        nodesVisited = 0
+
+        q = collections.deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+            currPoint = q.popleft()
+            nodesVisited += 1
+            for nei in adj[currPoint]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
         
-        visitSet = set()
-        def dfs(course):
-            if course in visitSet:
-                return False
-            if course not in courseDict or len(courseDict[course]) == 0:
-                return True
-            
-            visitSet.add(course)
-            for prereq in courseDict[course]:
-                if not dfs(prereq):
-                    return False
-            visitSet.remove(course)
-            courseDict[course].clear()
-            return True
-
-        for course in courseDict:
-            if not dfs(course):
-                return False
-        return True
-
-
-            
+        return nodesVisited == numCourses
