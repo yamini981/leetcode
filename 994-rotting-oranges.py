29f1e128e11c8ec9d91bc19a1b138270446d9d04
Mustafa Butt <coolmustafabutt9@gmail.com>
2025-03-04 13:19:35 -0500

    994. Rotting Oranges, BFS

--- 994-rotting-oranges.py
+++ 994-rotting-oranges.py
@@ -1,41 +1,47 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        # not optimal - should have done BFS instead!
-        
-        directions = [1, 0], [0, 1], [-1, 0], [0, -1]
         m = len(grid)
         n = len(grid[0])
-        print(m, n)
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        def dfsFreshToRotten(i, j, distTravelled, visited):
-            if grid[i][j] == 0:
-                return float('inf')
-            if grid[i][j] == 2:
-                return distTravelled
-            retVal = float('inf')
-            for dr, dc in directions:
-                if (i + dr < m and i + dr >= 0 and j + dc < n and j + dc >= 0
-                and (i + dr, j + dc) not in visited):
-                    print(i + dr, j + dc)
-                    visited.add((i + dr, j + dc))
-                    visitedCopy = set(visited)
-                    retVal = min(retVal, dfsFreshToRotten(i + dr, j + dc, distTravelled + 1, visitedCopy))
-            return retVal
-                    
+        # visited = set()
 
-        longestShortPath = 0
+        q = deque()
+        numFreshOranges = 0
+        # iterate through grid and add rotten oragnes to queue
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    visited = set([(i, j)])
-                    dfsRet = dfsFreshToRotten(i, j, 0, visited)
-                    print(i, j, dfsRet)
-                    if dfsRet == float('inf'):
-                        return -1
-                    else:
-                        longestShortPath = max(dfsRet, longestShortPath)
+                if grid[i][j] == 2:
+                    q.append((i, j))
+                elif grid[i][j] == 1:
+                    numFreshOranges += 1
+        
+        minutesElapsed = 0
+        if numFreshOranges == 0:
+            return 0
+        while q:
+            print(q)
+            for _ in range(len(q)):
+                currVal = q.popleft()
+                i, j = currVal
+                for dx, dy in directions:
+                    if (i + dx < m and i + dx >= 0 and
+                    j + dy < n and j + dy >= 0 and
+                    grid[i + dx][j + dy] == 1):
+                        grid[i + dx][j + dy] = 2
+                        numFreshOranges -= 1
+                        q.append((i + dx, j + dy))
+            minutesElapsed += 1
+            if numFreshOranges == 0:
+                return minutesElapsed
+        
+        return -1
 
-        return longestShortPath
 
-                
+
+
+
+
+        
+
+       
